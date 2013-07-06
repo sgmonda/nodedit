31ffd63bf8c2a3f3a4d9862bc0fd6a5ab86c97b3
Sergio García Mondaray <sgmonda@gmail.com>
2013-07-07 00:15:01 +0200

    Testing ansi-canvas as GUI system

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 nodedit
 =======
 
+# Installation
+
+1) Install node-canvas dependences: more info in https://github.com/LearnBoost/node-canvas/wiki/Installation---OSX and http://ethan.tira-thompson.com/Mac_OS_X_Ports.html
+
+2) Download this repo
+````
+git clone http://github.com/sgmonda/nodedit
+````
+
+3) Install npm dependences
+````
+npm install
+````
+
+
+
 A crazy idea
--- app.js
+++ app.js
@@ -9,12 +9,31 @@
 // DEPENDENCES =================================================================
 
 var settings = require('./settings.js');
-var gui = require('./gui.js');
+//var gui = require('./gui.js');
+var ac = require('ansi-canvas');
 
 // GLOBAL OBJECTS ==============================================================
 
+var canvas = ac();
+var context = canvas.getContext('2d');
 
 // MAIN LOGIC ==================================================================
+
+// draw a purple background
+context.fillStyle = 'purple';
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+// write some text
+context.fillStyle    = '#00f';
+context.font         = 'italic 15px sans-serif';
+context.textBaseline = 'top';
+context.fillText  ('Hello world!', 1, 1);
+context.font         = 'bold 20px sans-serif';
+context.strokeText('Hello world!', 1, 21);
+
+// IMPORTANT!!!
+// call `canvas.render()` when you're ready to flush the canvas to the terminal
+canvas.render();
 
 //gui.setTitle(settings.DEFAULT_TITLE);
 
@@ -22,9 +41,9 @@ var gui = require('./gui.js');
 
 // SIGNALS =====================================================================
 
-process.on('SIGINT', function () {
-	gui.close();
-});
+// process.on('SIGINT', function () {
+// 	gui.close();
+// });
 
 // TESTS =======================================================================
 
